f67d6e3047eeda0bed00fea21e025b44508ae55c
Cho Phan <callmechophan@gmail.com>
2024-04-10 21:52:44 +0700

    1.1 introduction

---
+++ introduction/main.sql
@@ -0,0 +1,75 @@
+/*
+ * data modeling
+ * - developing a data model that shows a visual representation of data elements and the relationship between them
+ * 
+ * three level database models
+ * - conceptual data model
+ * - logical data model
+ * - physical data model
+ * 
+ * how to structure tables to remove anomalies using "database normalization"
+ * - insert
+ * - update
+ * - delete
+ * 
+ * design a database model in an exercise
+ * 
+ * forward engineer
+ * - used to turn this model into a database schema
+ * 
+ * reverse engineer
+ * - create a data model / ER diagram (ERD) from an existing database
+ * 
+ * data warehouse
+ * - centralized data repository that aggregates, integrates, stores and processes large amounts of data from multiple sources
+ * 
+ * four key characteristics in data warehouse
+ * - subject-oriented
+ * - integrated
+ * - non-volatile
+ * - time variant
+ * 
+ * different forms of data that a data warehouse handles
+ * - structured data
+ * - semi-structured data
+ * - unstructured data
+ * 
+ * dimensional data modeling
+ * - dimensions and facts
+ * - stat and snowflake schemas
+ * 
+ * key steps for creating a model
+ * - choose the business process
+ * - choose the grain
+ * - choose the dimensions
+ * - chooes the facts
+ * 
+ * data analytics
+ * - preparing and processing aggregated data into useful and meaningful information
+ * 
+ * quantitative data
+ * - refers to numerical data
+ * 
+ * qualitative data
+ * - refers to non-numerical data
+ * 
+ * nominal scale
+ * ordinal scale
+ * interval scale
+ * ratio scale
+ * 
+ * data mining
+ * - detecting patterns in data
+ * 
+ * data mining models
+ * - classification analyis
+ * - associate rule
+ * - clustering analysis
+ * - regression analysis
+ * 
+ * tableau
+ * - data visualization tool
+ * - load and prepare data
+ * - filter and visualize data
+ * - create an interactive dashboard
+*/
