fddffb0fbaca178202d71011d0a66a2cb92a98cc
Cho Phan <callmechophan@gmail.com>
2024-04-12 14:08:57 +0700

    1.2.1 data modeling

---
+++ database_modeling/data_modeling/main.sql
@@ -0,0 +1,20 @@
+/*
+ * data modeling
+ * - the process of developing a visual representation of data elements and their relationships
+ * - it demonstrates how database system is structured
+ * - it help to understand how data is stored, accessed, updated, and queried within the database
+ * - it also ensures a consistent structure and high quality data
+ * 
+ * three level database models
+ * - conceptual data model
+ *     + present a "high level overview" of the database system through a visual repersentation of the "entities" it containss
+ *     + and their "relationship" to one another
+ * - logical data model
+ *     + build on conceptual data model, providing a more detailed overview of the entities in their respective relationship
+ *     + it identifies the attributes of each entity
+ * - physical data model
+ *     + used to create the internal SQL schema of the database, which is implemented in the database management system
+ * 
+ * notes:
+ * - https://online.visual-paradigm.com/knowledge/visual-modeling/conceptual-vs-logical-vs-physical-data-model/
+*/
