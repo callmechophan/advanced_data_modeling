06dce511fa73c72e5940f1b3e571e745f20098d4
Cho Phan <callmechophan@gmail.com>
2024-04-13 16:32:40 +0700

    1.3.1 introduction

---
+++ mysql_workbench/introduction/main.sql
@@ -0,0 +1,10 @@
+/*
+ * MySQL Workbench
+ * - unified visual tool for database modeling and management
+ * 
+ * key features
+ * - open-source and cross platform
+ * - simplifies database design and maintenance, and offers a visual SQL editor and other tools
+ * - autocomplete and highlighting features for writing SQL
+ * - facilitates data migration
+*/
