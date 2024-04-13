0411420d526ab18a2b5badc761aa549cdce48741
Cho Phan <callmechophan@gmail.com>
2024-04-14 00:58:58 +0700

    2.1.2 elt

---
+++ data_warehouse/etl/main.sql
@@ -0,0 +1,18 @@
+/*
+ * extract
+ * - select valid and relevant data
+ * - remove duplicated data
+ * - exclude spam and corrupted data
+ * 
+ * transform
+ * - format data
+ * - clean data
+ * - prepare data
+ * - organize data
+ * - integrate data
+ * 
+ * load
+ * - initial load
+ * - incremental load
+ * - refresh load
+*/
