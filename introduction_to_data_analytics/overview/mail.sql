b01931bff3f527f251bca1d6c45ae4f21f074644
Cho Phan <callmechophan@gmail.com>
2024-04-15 00:28:05 +0700

    3.1.1 overview of data analytics

---
+++ introduction_to_data_analytics/overview/mail.sql
@@ -0,0 +1,48 @@
+/*
+ * data analytics
+ * - the process of analyzing data to derive useful information and valuable insights
+ * 
+ * serveral key types of data analysis
+ * - descriptive data analysis: presents data in a descriptive format
+ * - exploratory data analysis: is used to establish a relationship between different variables
+ * - inferential data analysis: focuses on a small sample of data to make inferences
+ * - predictive data analysis: identifies patterns and data to make predictions about future performance
+ * - causal data analysis: explores cause and effect between variables
+ * 
+ * kind of measurements
+ * - quantitative
+ *     + refers to numerical data
+ *     + data that can be counted or quantified
+ *     + EX:
+ *         . average number of customers who make purchases each day
+ *         . average cost of each purchase made
+ * - qualitative
+ *     + refers to non-numerical data
+ *     + textual and descriptive data
+ *     + EX: category names or description of products like furniture of office supplies
+ * 
+ * four different measurement scales
+ * - nominal scale (qualitative)
+ *     + describes the identity property of non-numerical data
+ *     + EX:
+ *         . identify products in their stock like a chair or a desk
+ *         . each product is one nominal unit of data
+ * 
+ * - ordinal (order) scale (qualitative)
+ *     + places data in a specific ranked order
+ *     +  it doesn't include decisive criteria to determine the difference between the data elements
+ *     + EX:
+ *         . rank chairs using ratings values
+ *         . top quality product (1st), very good product (2nd), good product (3rd)
+ *         . no precise criteria that determines the measurement between each value
+ * - interval scale
+ *     + includes properties of the nominal and ordinal scales. 
+ *     + identifies the differences between data points
+ *     + can also contain both positive, negative numbers, and zero
+ *     + EX: can use the interval scale to provide feedback on products from 10 to -10
+ * - ratio scale (quantitative)
+ *     + includes properties from nominal, ordinal, and interval scale
+ *     + it defines the identity of the data classifies the data in order and marks clear intervals
+ *     + it holds an absolute value of zero
+ *     + EX: can use the ratio scale to mark the weight of products
+*/
